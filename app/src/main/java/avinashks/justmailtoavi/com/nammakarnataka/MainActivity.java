02b5e8b51f4772963339f151cb300795bcd26010
Avinash <justmailtoavi@gmail.com>
2016-08-14 11:28:18 +0530

    Added Splash Screen.

--- app/src/main/java/avinashks/justmailtoavi/com/nammakarnataka/MainActivity.java
+++ app/src/main/java/avinashks/justmailtoavi/com/nammakarnataka/MainActivity.java
@@ -2,6 +2,8 @@ package avinashks.justmailtoavi.com.nammakarnataka;
 
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -16,6 +18,7 @@ import com.daimajia.slider.library.SliderTypes.BaseSliderView;
 import com.daimajia.slider.library.SliderTypes.TextSliderView;
 
 import java.util.HashMap;
+
 
 
 public class MainActivity extends AppCompatActivity
@@ -35,8 +38,10 @@ public class MainActivity extends AppCompatActivity
 
         mDemoSlider = (SliderLayout) findViewById(R.id.mainActivitySlider);
         final HashMap<String,Integer> file_maps = new HashMap<>();
-        file_maps.put("Stone Chariot",R.drawable.vijaynagar);
+        file_maps.put("Hampi",R.drawable.vijaynagar);
         file_maps.put("Mysuru Palace",R.drawable.mysuru_palace);
+        file_maps.put("Jog Falls",R.drawable.jogfalls);
+        file_maps.put("Bandipur National Park",R.drawable.bandipur);
 
         for(String name : file_maps.keySet()){
             TextSliderView textSliderView = new TextSliderView(getApplicationContext());
@@ -54,12 +59,10 @@ public class MainActivity extends AppCompatActivity
             mDemoSlider.addSlider(textSliderView);
         }
 
-
-
         mDemoSlider.setPresetTransformer(SliderLayout.Transformer.DepthPage);
         mDemoSlider.setPresetIndicator(SliderLayout.PresetIndicators.Center_Bottom);
         mDemoSlider.setCustomAnimation(new DescriptionAnimation());
-        mDemoSlider.setDuration(7000);
+        mDemoSlider.setDuration(6000);
 
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
@@ -107,10 +110,31 @@ public class MainActivity extends AppCompatActivity
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
-        // Handle navigation view item clicks here.
+        Fragment fragment;
         int id = item.getItemId();
 
-        if (id == R.id.nav_dams) {
+        switch (id){
+            case R.id.nav_dams:
+                                fragment = new damsFragment();
+                                FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+                                ft.replace(R.id.content_main,fragment);
+                                ft.commit();
+                                break;
+
+            case R.id.nav_temples:
+                break;
+
+            case R.id.nav_beaches:
+                break;
+
+            case R.id.nav_hillstations:
+                break;
+
+            case R.id.nav_trekking:
+                break;
+
+            case R.id.nav_waterfalls:
+                break;
 
 
         }
---
+++ app/src/main/java/avinashks/justmailtoavi/com/nammakarnataka/damsFragment.java
@@ -0,0 +1,22 @@
+package avinashks.justmailtoavi.com.nammakarnataka;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class damsFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_dams, container, false);
+
+
+    }
+
+
+
+}
