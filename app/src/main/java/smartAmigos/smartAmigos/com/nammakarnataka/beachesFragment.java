f17ecb719d917a27f8eeb7adb17461a1ac5e0516
CharanNK <charkeet@gmail.com>
2016-08-25 21:27:06 +0530

    Resolved comments error

--- app/src/main/java/smartAmigos/smartAmigos/com/nammakarnataka/beachesFragment.java
+++ app/src/main/java/smartAmigos/smartAmigos/com/nammakarnataka/beachesFragment.java
@@ -46,7 +46,7 @@ public class beachesFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_dams,container,false);
+        View view = inflater.inflate(R.layout.fragment_beaches,container,false);
 
         comment_box = (EditText)view.findViewById(R.id.comment_box);
         user_Name = (TextView) view.findViewById(R.id.user_name);
