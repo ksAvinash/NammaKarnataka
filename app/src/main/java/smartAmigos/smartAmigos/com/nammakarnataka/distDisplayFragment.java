6cfbb55ff49642b1bec8293f751e2227049557b4
Avinash <justmailtoavi@gmail.com>
2016-09-02 22:04:19 +0530

    JInka Chika BIG thing..

--- app/src/main/java/smartAmigos/smartAmigos/com/nammakarnataka/distDisplayFragment.java
+++ app/src/main/java/smartAmigos/smartAmigos/com/nammakarnataka/distDisplayFragment.java
@@ -6,6 +6,7 @@ import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+
 import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
