cb0fb85e6ae96ea64bdf4948040363f83fe522d9
AshwinChandlapur <ashwinyga@rocketmail.com>
2016-09-02 20:39:07 +0530

    Feedback Added 1.Give us a 5 2.Feedback
    Full Ready.

--- app/src/main/java/smartAmigos/smartAmigos/com/nammakarnataka/feedbacks.java
+++ app/src/main/java/smartAmigos/smartAmigos/com/nammakarnataka/feedbacks.java
@@ -21,7 +21,9 @@ public class feedbacks extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        android.support.v7.app.ActionBar AB=getSupportActionBar();
+        AB.hide();
+        //requestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_feedbacks);
