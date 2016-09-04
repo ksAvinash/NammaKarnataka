ae308098b44a07a9db33d74c1267ad184ec2b3aa
CharanNK <charkeet@gmail.com>
2016-09-05 01:06:09 +0530

    Implemented google login

---
+++ app/src/main/java/smartAmigos/smartAmigos/com/nammakarnataka/NotifHandler.java
@@ -0,0 +1,10 @@
+package smartAmigos.smartAmigos.com.nammakarnataka;
+
+import android.app.Activity;
+
+/**
+ * Created by CHARAN on 9/5/2016.
+ */
+public class NotifHandler extends Activity {
+
+}
