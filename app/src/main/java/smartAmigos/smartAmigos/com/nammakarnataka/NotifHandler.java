d700a9ff9183a7a9e3626f76d91948bb12cdd116
AshwinChandlapur <ashwinyga@rocketmail.com>
2016-09-06 00:25:24 +0530

    NotifHandler Class .

---
+++ app/src/main/java/smartAmigos/smartAmigos/com/nammakarnataka/NotifHandler.java
@@ -0,0 +1,15 @@
+package smartAmigos.smartAmigos.com.nammakarnataka;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by ${Ashwin_Chandlapur} on 9/6/2016.
+ */
+public class NotifHandler extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_general);
+    }
+}
